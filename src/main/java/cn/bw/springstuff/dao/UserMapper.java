9c5ea42f2f87899e7d74bf71db30ada4265904c7
Richard.Wu <120793495@qq.com>
2019-05-10 11:32:46 +0800

    first db

---
+++ src/main/java/cn/bw/springstuff/dao/UserMapper.java
@@ -0,0 +1,12 @@
+package cn.bw.springstuff.dao;
+
+/**
+ * @description:
+ * @auther: bian.wu
+ * @date: 2019/5/10 11:31
+ */
+public interface UserMapper {
+
+
+
+}
---
+++ src/main/java/cn/bw/springstuff/entity/User.java
@@ -0,0 +1,18 @@
+package cn.bw.springstuff.entity;
+
+import lombok.Data;
+
+/**
+ * @description:
+ * @auther: bian.wu
+ * @date: 2019/5/10 11:32
+ */
+
+@Data
+public class User {
+
+    private long id;
+    private String name;
+    
+
+}
